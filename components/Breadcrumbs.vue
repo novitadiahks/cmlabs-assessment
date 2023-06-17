c2092c6546c1c219ddef2ca259c236e435822ff9
novitadiahks <50206884+novitadiahks@users.noreply.github.com>
2023-06-17 10:47:55 +0700

    update breadcrumbs

--- components/Breadcrumbs.vue
+++ components/Breadcrumbs.vue
@@ -2,47 +2,42 @@
   <div class="mt-8 flex items-center flex-wrap">
     <ul class="flex items-center">
       <li class="inline-flex items-center">
-        <NuxtLink
-          to="/"
-          class="flex items-center gap-x-1 text-gray-600 hover:text-blue-500"
-        >
+        <NuxtLink to="/" class="flex items-center gap-x-1 text-gray-600 hover:text-blue-500">
           <Icon icon="ic:round-home" />Home
         </NuxtLink>
         <Icon class="mx-2 text-gray-400" icon="mingcute:right-line" />
       </li>
 
       <li class="inline-flex items-center">
-        <NuxtLink
-          to="/"
-          class="flex items-center gap-x-1 text-gray-600 hover:text-blue-500"
-        >
+        <NuxtLink to="/" class="flex items-center gap-x-1 text-gray-600 hover:text-blue-500">
           Foods
         </NuxtLink>
         <Icon class="mx-2 text-gray-400" icon="mingcute:right-line" />
       </li>
 
-      <li class="inline-flex items-center" v-for="(breadcrumb, index) in breadcrumbs" :key="index">
-        <NuxtLink
-          :to="breadcrumb.to"
-          class="flex items-center gap-x-1 text-gray-400 hover:text-blue-500"
-        >
-          {{ breadcrumb.text }}
+      <li v-for="(breadcrumb, index) in breadcrumbs" :key="index" class="inline-flex items-center">
+        <NuxtLink :to="breadcrumb.path" class="flex items-center gap-x-1 text-gray-400 hover:text-blue-500">
+          {{ breadcrumb.label }}
         </NuxtLink>
       </li>
     </ul>
   </div>
 </template>
 
+
 <script setup lang="ts">
 import { Icon } from "@iconify/vue";
-const route = useRoute();
+const apiData = ref([]);
 const breadcrumbs = computed(() => {
-  const segments = route.path.split("/").filter((segment) => segment);
-  const breadcrumbs = segments.map((segment, index) => ({
-    text: segment,
-    to: `/${segments.slice(0, index + 1).join("/")}`,
+  const route = useRoute();
+
+  const breadcrumb = Object.entries(route.query).map(([key, value]) => ({
+    label: value,
+    path: `${route.path}?${key}=${value}`,
   }));
-  return breadcrumbs;
+
+  // Menggabungkan data dari API dengan breadcrumb yang ada
+  return [...breadcrumb, ...apiData.value];
 });
 </script>
 
